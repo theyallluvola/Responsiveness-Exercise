2dada5bcff078205cd5ca1ba10bb7d4395818188
olaoluwaigun <igunolly@gmail.com>
2025-02-18 07:58:03 -0500

    Done

---
+++ index.js
@@ -0,0 +1,8 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.classList.add('show')
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.classList.remove('show')
+}
